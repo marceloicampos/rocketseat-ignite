967e0c069fd4ded83ed6ebc9abc7aea78a8b55e1
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-04-20 20:05:50 -0300

    Usando Esatdo de Componetes - useState

---
+++ reactjs/01-github-explorer/src/components/Counter.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+// useState é um hook
+
+export function Counter() {
+    const [counter, setCounter] = useState(0)
+    // let counter = 0
+    function Increment() {
+        setCounter(counter + 1)
+        // counter += 1
+        // counter ++
+        // counter = counter + 1
+        // console.log('increment');
+    }
+    function Reset() {
+        setCounter(0)
+    }
+    return (
+        <div>
+            <h2>Clique abaixo para Incrementar o Contador {counter}</h2>
+            <button type="button" onClick={Increment}>
+                Increment
+            </button>
+            <h2>ou Clique abaixo para Zera o Contador</h2>
+            <button type="button" onClick={Reset}>
+                Reset
+            </button>
+        </div>
+    )
+}
--- reactjs/01-github-explorer/src/components/RepositoryItem.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryItem.jsx
@@ -7,7 +7,7 @@ export function RepositoryItem(props) {
             <strong>{props.repository.name ?? 'Default'}</strong>
             <p>{props.repository.description ?? 'Default'}</p>
             <a href={props.repository.link ?? 'Default'} target="_blank">
-                {props.repository.link_name}
+                {props.repository.link_name ?? 'Default'}
             </a>
         </li>
     )
