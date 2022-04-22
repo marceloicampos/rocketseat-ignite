15d3ede7e8aea2203b16cbaa5cc59c7c43b0cc4b
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-04-22 10:33:55 -0300

    listando repositórios com useEffect e inserindo o avatar

--- reactjs/01-github-explorer/src/components/RepositoryItem.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryItem.jsx
@@ -6,8 +6,8 @@ export function RepositoryItem(props) {
         <li>
             <strong>{props.repository.name ?? 'Default'}</strong>
             <p>{props.repository.description ?? 'Default'}</p>
-            <a href={props.repository.link ?? 'Default'} target="_blank">
-                {props.repository.link_name ?? 'Default'}
+            <a href={props.repository.html_url ?? 'Default'} target="_blank">
+                {props.repository.full_name ?? 'Default'}
             </a>
         </li>
     )
--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,22 +1,39 @@
 import { RepositoryItem } from './RepositoryItem'
 import '../styles/repositories.scss'
+import { useState, useEffect } from 'react'
 
-const repository = {
-    name: 'repo name',
-    description: 'repo description',
-    link: 'https://github.com',
-    link_name: 'repo link'
-}
+// const repository = {
+//     name: 'repo name',
+//     description: 'repo description',
+//     link: 'https://github.com',
+//     link_name: 'repo link'
+// }
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/marceloicampos/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+    // console.log(repositories)
+    // o useEffect recebe dois params: qual função vamos executar e quando vamos executar a função
     return (
         <section className="repository-list">
+            <h1>Git Hub - marceloicampos</h1>
+            <a href="https://github.com/marceloicampos" target="_blank">
+                <img src="https://avatars.githubusercontent.com/u/71357926?v=4" alt="foto avatar git hub marcelo campos" />
+            </a>
             <h1>Lista de Repositórios - marceloicampos</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
+
+                {/* {repositories.map(repository => (<RepositoryItem repository={repository} />))} */}
+
+                {/* {repositories.map(repository => <RepositoryItem repository={repository} />)} */}
             </ul>
         </section>
     )
