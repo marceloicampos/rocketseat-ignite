13025fbb2cebf800bab63353131bb32e9d385e3c
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-04-23 19:23:16 -0300

    inserido fontes do projeto usando o google fotnts

--- reactjs/02-dtmoney/src/styles/global.ts
+++ reactjs/02-dtmoney/src/styles/global.ts
@@ -13,7 +13,7 @@ export const GlobalStyle = createGlobalStyle`
     --text-body: #969cb3;
 
     --shape: #ffffff;
-    
+
     --background: #f0f2f5;
   }
   
@@ -39,6 +39,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
