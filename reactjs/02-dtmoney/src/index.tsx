f790ef999092cb6ec29e9a3b4066984cccaba1d5
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-04-23 19:21:59 -0300

    atualizando comentários do index.tsx

--- reactjs/02-dtmoney/src/index.tsx
+++ reactjs/02-dtmoney/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
 // import { StyledApp } from './StyledApp';
-// Componente StyledApp dentro do render
+// para fins de testes, se quiser, pode inserir o componente StyledApp dentro do root.render
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
