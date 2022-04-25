8fcf69df7e5324104084348f1c59b019e7bbd5a3
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-04-25 12:38:06 -0300

    using mirageJS with Fake API

--- reactjs/02-dtmoney/src/index.tsx
+++ reactjs/02-dtmoney/src/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs'
 import { App } from './App';
 
 // import { StyledApp } from './StyledApp';
 // para fins de testes, se quiser, pode inserir o componente StyledApp dentro do root.render
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id:1,
+          title: 'Fix Site',
+          amount: 400,
+          type: 'deposit',
+          category: 'Work',
+          createdDate: new Date()
+        }
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
