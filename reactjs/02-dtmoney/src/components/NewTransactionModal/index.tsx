986a4dababe334944b2f8333821af5599c05aa4e
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-05-24 16:37:29 -0300

    Modal Styles

--- reactjs/02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
-import { ContainerModal } from "./styles";
+import closeSvg from '../../assets/close.svg'
+import { ContainerFormModal } from "./styles";
 import Modal from 'react-modal'
 
 interface NewTransactionModalProps {
@@ -12,10 +13,25 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName='react-modal-overlay'
+      className='react-modal-content'
+    // modal tem estilo globa
     >
-      <ContainerModal>
+      <button
+        type='button'
+        onClick={onRequestClose}
+        className='react-modal-close'
+      // fechar modal tem estilo global
+      >
+        <img src={closeSvg} alt="botão de fechar o Modal" />
+      </button>
+      <ContainerFormModal>
         <h2>Cadastrar Transação</h2>
-      </ContainerModal>
+        <input placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </ContainerFormModal>
     </Modal>
   )
 }
--- reactjs/02-dtmoney/src/components/NewTransactionModal/styles.ts
+++ reactjs/02-dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,7 +1,60 @@
 import styled from "styled-components"
 
-export const ContainerModal = styled.div`
+export const ContainerFormModal = styled.form`
 h2 {
-  background-color: red;
+  color: var(--text-title);
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
+}
+
+input {
+  width: 100%;
+  padding: 0 1.5rem;
+  height: 4rem;
+  border-radius: 0.25rem;
+  background: #e7e9ee;
+  border: 1px solid #d7d7d7;
+  font-weight: 400;
+  font-size: 1rem;
+  
+  &::placeholder {
+    color: var(--text-body);
+  }
+
+  & + input {
+    margin-top: 1rem;
+  };
+}
+
+/* hide arrows of input number Works for Firefox */
+input[type="number"] {
+  appearance: none;
+  -moz-appearance: textfield;
+}
+
+/* hide arrows of input number Works for Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+button[type="submit"] {
+  background: var(--green);
+  width: 100%;
+  padding: 0 1.5rem;
+  height: 4rem;
+  color: #ffffff;
+  border-radius: 0.25rem;
+  border: 0;
+  font-size: 1rem;
+  margin-top: 1.5rem;
+  font-weight: 600;
+
+  transition: filter 0.2s;
+
+  &:hover {
+    filter: brightness(0.9);
+  }
 }
 `
