1dc895ac92b1bca212fe83c3b2a4990e598a2d99
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-05-24 10:30:06 -0300

    Create Modal Component

--- reactjs/02-dtmoney/src/components/Header/index.tsx
+++ reactjs/02-dtmoney/src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import logoSvg from '../../assets/logo.svg'
 import { ContainerHeader, ContentHeaderDiv } from './styles'
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <ContainerHeader>
       <ContentHeaderDiv>
         <img src={logoSvg} alt="logo dtMoney" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova Transação
         </button>
       </ContentHeaderDiv>
---
+++ reactjs/02-dtmoney/src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const ContainerModal = styled.div`
+h2 {
+  background-color: red;
+}
+`
